6f2f6cb9932335395c6835f9ded3f773ebd6cda6
Jetso Analin <jetyjetso@gmail.com>
2021-12-14 13:13:25 +0530

    Validation added

--- api/src/middleware/awaitHandlerFactory.middleware.js
+++ api/src/middleware/awaitHandlerFactory.middleware.js
@@ -1,8 +1,8 @@
 const awaitHandlerFactory = (middleware) => {
-    return async (req,res,next)=>{
-        try{
-            await middleware(req,res,next)
-        }catch(err){
+    return async (req, res, next) => {
+        try {
+            await middleware(req, res, next)
+        } catch (err) {
             next(err)
         }
     }
